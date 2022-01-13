97a73fd5fdff218e31f62388eab58cedff96f805
Dinesh Kumar <mr.dineshdkd@gmail.com>
2022-01-13 08:29:54 +0530

    Added: Trigonometric Function

---
+++ src/main/java/com/bridgelab/testing/logicalprograms/Trig.java
@@ -0,0 +1,34 @@
+package com.bridgelab.testing.logicalprograms;
+
+import java.util.Scanner;
+
+public class Trig {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(
+				"Enter:-\n1. To Get Value Of Sin()" + "\n2. To Get Value Of Cos()" + "\n3. To Get Value Of Radians");
+		int choice = scanner.nextInt();
+		menu(choice);
+		scanner.close();
+	}
+
+	private static void menu(int choice) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Angle In Degree: ");
+		double angle = scanner.nextDouble();
+		switch (choice) {
+		case 1:
+			System.out.println(Math.sin(angle));
+			break;
+		case 2:
+			System.out.println(Math.cos(angle));
+			break;
+		case 3:
+			System.out.println(Math.toRadians(angle));
+			break;
+		default:
+			System.out.println("Invalid Choice !!");
+		}
+		scanner.close();
+	}
+}
