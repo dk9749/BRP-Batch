49ae781c02b4598f52c9df7a20021c23b7e9e418
Dinesh Kumar <mr.dineshdkd@gmail.com>
2022-01-13 19:02:24 +0530

    Added: Harmonic Number Calculation

---
+++ src/main/java/com/bridgelab/testing/logicalprograms/HarmonicNumber.java
@@ -0,0 +1,21 @@
+package com.bridgelab.testing.logicalprograms;
+
+import java.util.Scanner;
+
+public class HarmonicNumber {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Number: ");
+		double number = scanner.nextInt();
+		System.out.println(harmonicNumber(number));
+		scanner.close();
+	}
+
+	private static double harmonicNumber(double number) {
+		double harmonicNumber = 0;
+		for (double i = 1; i <= number; i++) {
+			harmonicNumber += (1 / i);
+		}
+		return harmonicNumber;
+	}
+}
