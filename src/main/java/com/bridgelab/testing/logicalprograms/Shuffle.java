95cf99efa8f15b0f05db8ab0024f06c9968ae869
Dinesh Kumar <mr.dineshdkd@gmail.com>
2022-02-22 08:36:40 +0530

    Program that arrange a statement according to integer present in the each word of the statement

--- src/main/java/com/bridgelab/testing/logicalprograms/Shuffle.java
+++ src/main/java/com/bridgelab/testing/logicalprograms/Shuffle.java
@@ -1,41 +1,45 @@
 package com.bridgelab.testing.logicalprograms;
 
+import java.util.Scanner;
+
+//Example: input=> is2 sentence4 This1 a3
+//		   output=> This is a sentence
 public class Shuffle {
 	public static void main(String[] args) {
-//	Scanner scanner = new Scanner(System.in);
-//	System.out.println("Enter Statement: ");
-//	String statement = scanner.nextLine();
-		shuffle("is2 sentence4 This1 a3");
-//	scanner.close();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Statement: ");
+		String statement = scanner.nextLine();
+		shuffle(statement);
+		scanner.close();
 	}
 
-	private static void shuffle(String s) {
-		String[] a = sort(s);
-		for (int i = 0; i < a.length; i++) {
-			int length = a[i].length();
-			a[i] = a[i].substring(0, length - 1);
+	private static void shuffle(String string) {
+		String[] array = sort(string);
+		for (int i = 0; i < array.length; i++) {
+			int length = array[i].length();
+			array[i] = array[i].substring(0, length - 1);
 		}
-		for (String d : a) {
+		for (String d : array) {
 			System.out.print(d + " ");
 		}
 	}
 
-	private static String[] sort(String s) {
-		String[] a = s.split(" ");
+	private static String[] sort(String string) {
+		String[] array = string.split(" ");
 		String temp;
-		for (int i = 0; i < a.length; i++) {
-			for (int j = i + 1; j < a.length; j++) {
-				if (check(a[i]) > check(a[j])) {
-					temp = a[i];
-					a[i] = a[j];
-					a[j] = temp;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (check(array[i]) > check(array[j])) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
 				}
 			}
 		}
-		return a;
+		return array;
 	}
 
-	private static int check(String s) {
-		return s.charAt(s.length() - 1);
+	private static int check(String string) {
+		return string.charAt(string.length() - 1);
 	}
 }
