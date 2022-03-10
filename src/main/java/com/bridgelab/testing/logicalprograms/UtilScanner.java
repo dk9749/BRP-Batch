51b69020f94a89961889d2707677a18630d1cae4
Dinesh Kumar <mr.dineshdkd@gmail.com>
2022-03-10 11:52:08 +0530

    Refactored UtilScanner Class

--- src/main/java/com/bridgelab/testing/logicalprograms/UtilScanner.java
+++ src/main/java/com/bridgelab/testing/logicalprograms/UtilScanner.java
@@ -1,5 +1,6 @@
 package com.bridgelab.testing.logicalprograms;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UtilScanner {
@@ -7,12 +8,44 @@ public class UtilScanner {
 
 	public static int getInt(String messege) {
 		messege(messege);
-		return SCANNER.nextInt();
+		int i = 0;
+		do {
+			try {
+				i = SCANNER.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Incorrect Input !! please try again.");
+			}
+			SCANNER.nextLine();
+		} while (i <= 0);
+		return i;
+	}
+
+	public static long getLong(String messege) {
+		messege(messege);
+		long i = 0;
+		do {
+			try {
+				i = SCANNER.nextLong();
+			} catch (InputMismatchException e) {
+				System.out.println("Incorrect Input !! please try again.");
+			}
+			SCANNER.nextLine();
+		} while (i <= 0);
+		return i;
 	}
 
 	public static double getDouble(String messege) {
 		messege(messege);
-		return SCANNER.nextDouble();
+		double i = 0;
+		do {
+			try {
+				i = SCANNER.nextDouble();
+			} catch (InputMismatchException e) {
+				System.out.println("Incorrect Input !! please try again.");
+			}
+			SCANNER.nextLine();
+		} while (i <= 0);
+		return i;
 	}
 
 	public static String getString(String messege) {
